0415e0eec25cec1c9ae3bb26ee2bb13607fb45be
zhangronghua <qq798435167@gmail.com>
2018-06-29 11:50:47 +0800

    update

---
+++ app.js
@@ -0,0 +1,60 @@
+const Koa = require('koa');
+const koarouter = require('koa-router');
+const bodyparser = require('koa-bodyparser');
+
+const app = new Koa();
+const router = koarouter();
+
+//添加异步处理函数（处理中间件），函数会顺序执行
+app.use(async (ctx,next)=>{
+    console.log(`Process: ${ctx.request.method} ${ctx.request.url}`);
+    await next(); //调用下一个函数
+})
+
+app.use(async (ctx,next)=>{
+    const start_time = new Date().getTime();
+    await next();
+    const ms = new Date().getTime() - start_time;
+    console.log(`Time cost: ${ms}`)
+})
+
+// app.use(async (ctx,next)=>{
+//     await next();
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = '<h1>Hello,KOA</h1>';
+// })
+
+// app.use(formidable());
+// 在使用router前先添加bodyparser
+app.use(bodyparser());
+
+router.get('/hello/:name',async (ctx,next)=>{
+    const name = ctx.params.name;
+    ctx.response.body = `<h1>Hello,${name}</h1>`;
+})
+
+router.get('/',async (ctx,next)=>{
+    const name = ctx.params.name;
+    ctx.response.body = `<h1>Welcome Koa</h1>`;
+})
+
+// 处理post请求
+router.post('/sign',async (ctx,next)=>{
+    console.log(ctx.request.body);
+    const name = ctx.request.body.name || "";
+    const password = ctx.request.body.password || "";
+
+    console.log(`sign in with name: ${name} password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+})
+
+app.use(router.routes());
+
+
+app.listen(3000);
+console.log('app started at port 3000');
