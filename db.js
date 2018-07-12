d777278339daf3cf17b59214a9cfd8e59dad8255
张荣划 <zhangronghua@quseit.com>
2018-07-12 16:56:35 +0800

    update

---
+++ db.js
@@ -0,0 +1,30 @@
+const mysql      = require('mysql');
+
+const pool  = mysql.createPool({
+    host     : 'localhost',   // 数据库地址
+    user     : 'root',    // 数据库用户
+    password : 'zhang1107' ,  // 数据库密码
+    database : 'test'  // 选中数据库
+  });
+
+const query = (sql,values)=>{
+    return new Promise((resolve,reject)=>{
+        pool.getConnection(function(err, connection) {
+            if (err) {
+              reject( err )
+            } else {
+              connection.query(sql, values, ( err, rows) => {
+      
+                if ( err ) {
+                  reject( err )
+                } else {
+                  resolve( rows )
+                }
+                connection.release()
+              })
+            }
+          })
+    });
+}
+
+module.exports = { query }
